e6071df722ae96ef4c39b449ad2c69c07842f943
Celiana-Rocha <celianarocha223@gamil.com>
2024-08-08 20:51:27 -0300

    automation

---
+++ canal.py
@@ -0,0 +1,47 @@
+import pyautogui as pa
+import time
+pa.PAUSE = 1
+
+pa.press('win')
+pa.write('chrome')
+pa.press('ENTER')
+pa.write("https://youtu.be/757G_El3ABI?si=XG1zNXKQHRoCGUhU")
+pa.press('ENTER')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# time.sleep(4)
+# pa.click(x=779, y=154)
+# pa.write("https://youtu.be/PnqdN15KqME?si=zKGVJ526RGANAUdT")
+
+
